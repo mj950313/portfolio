113d7ddc23f98c3caf51f3fc1616dec6a6675955
김민재 <kimminjae@gimminjaeui-MacBookPro.local>
2024-04-15 03:49:52 +0900

    contact push

--- src/layout/ContactBox.jsx
+++ src/layout/ContactBox.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { CiMail } from "react-icons/ci";
+import { Link } from "react-scroll";
 
 export default function ContactBox() {
   return (
     <div className="relative flex justify-center items-center duration-500 w-[100px] h-[100px] border-t-primary border-l-transparent border-b-transparent border-r-transparent border-t-[100px] border-r-[100px] border-b-0 border-l-0 xl:border-t-[174px] xl:border-r-[174px] xl:border-b-0 xl:border-l-0">
-      <div className="absolute flex flex-col items-center justfy-center duration-500 top-[-80px] left-1 xl:top-[-140px] xl:left-1 rotate-[-45deg]">
+      <Link
+        to="contact"
+        className="cursor-pointer absolute flex flex-col items-center justfy-center duration-500 top-[-80px] left-1 xl:top-[-140px] xl:left-1 rotate-[-45deg]"
+      >
         <span className="text-lg xl:text-3xl duration-500">
           <CiMail />
         </span>
         <div className="text-md xl:text-3xl duration-500 font-semibold leading-3">
           Contact
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import Title from "../components/Title";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { scrollSpy } from "react-scroll";
 
 export default function Contact() {
   return (
     <div>
       <Title title="C o n t a c t" />
-      Contact
+      <div
+        style={{ backgroundImage: "url(/images/left.png)" }}
+        className="bg-center bg-[#0e0e0e] bg-cover flex flex-col items-center"
+      >
+        <div className="flex flex-col py-8">
+          <p className="flex items-center text-xl gap-2">
+            <MdOutlineMailOutline className="text-primary" />
+            Email : mj950313@naver.com
+          </p>
+          <p className="flex items-center text-xl gap-2">
+            <FaPhoneAlt className="text-primary" />
+            Phone : 010 9809 3009
+          </p>
+        </div>
+        <a href="https://github.com/mj950313" className=" text-7xl p-5 mb-10">
+          <FaGithub />
+        </a>
+      </div>
     </div>
   );
 }
